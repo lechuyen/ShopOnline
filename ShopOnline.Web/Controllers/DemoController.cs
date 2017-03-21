7027fe83140ce1c1c30d49877006c144511bb16a
CHUYEN\Chuyen <aaaaaaaaaa>
2017-03-21 23:57:03 +0700

    aaaaaaaaaaaaaaaa

---
+++ ShopOnline.Web/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnline.Web.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult dc()
+        {
+            return View();
+        }
+    }
+}
